d7b167f24e83261907667486f069e171d08e3143
seanmoylan <sean.cheezy.moylan@gmail.com>
2019-09-15 16:28:47 +0100

    First draft of Collatz is done

---
+++ collatz.py
@@ -0,0 +1,17 @@
+# The number we will perform the Collatz oporations on.
+n = 20
+
+
+# Keep looping until we reach 1.
+# Note: this assumes the collatz conjecture is true
+while n != 1:
+    # Print the current calue of n.
+    print(n)
+    # Check if n is even.
+    if n % 2 == 0:
+        n = n / 2
+    else:
+        n = (3 * n) + 1
+
+# Finally print the 1.
+print(n)
